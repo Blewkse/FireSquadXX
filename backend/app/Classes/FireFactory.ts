9194ff1a167885636cd8e5101d127888808bd787
ClementObspher <144014423+ClementObspher@users.noreply.github.com>
2023-12-21 13:54:50 +0100

    :sparkles: fire factory

---
+++ backend/app/Classes/FireFactory.ts
@@ -0,0 +1,13 @@
+class FireFactory {
+  static create(position: { x: number; y: number }) {
+    const fire = new Fire();
+    fire.positionsList = [position];
+    fire.robotsList = [];
+    return fire;
+  }
+
+  static update(fire: Fire, newPosition: { x: number; y: number }) {
+    const { positionsList } = fire;
+    positionsList.push(newPosition);
+  }
+}
