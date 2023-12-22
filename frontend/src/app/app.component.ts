df16a8c0a8f1e4ec2d7d2467f6d2148156362779
brice.matias.e <brice.matias.e@thalesdigital.io>
2023-12-22 16:01:02 +0100

    init map

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MapComponent } from "./components/map/map.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'frontend';
+  constructor() {
+
+  }
 }
---
+++ frontend/src/app/components/map/map.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  standalone: true,
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements OnInit {
+
+  private x: number = 250;
+  private y: number = 250;
+  private dx: number = 2;
+  private dy: number = 1;
+
+  ngOnInit(): void {
+    this.drawBackground();
+  }
+
+  drawBackground() {
+    const canvas = document.getElementById('mapCanvas') as HTMLCanvasElement;
+    const context = canvas?.getContext('2d');
+
+    if (canvas && context) {
+      const img = new Image();
+      img.src = '/assets/Pixilframe.png';
+
+      img.onload = () => {
+        context.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+        const x = 250;
+        const y = 250;
+
+        context.beginPath();
+        context.arc(x, y, 5, 0, 2 * Math.PI);
+        context.fillStyle = 'red';
+        context.fill();
+        context.closePath();
+      };
+
+      img.addEventListener('error', (event) => {
+        console.error("Error loading image:", event);
+      });
+    } else {
+      console.error("Canvas element or context not found.");
+    }
+  }
+
+}
