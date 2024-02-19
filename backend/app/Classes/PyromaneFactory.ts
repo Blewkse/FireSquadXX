d862f8f7fa55749f43b8854645a225f7afdf434a
El Luco <saintsuperylucas@gmail.com>
2024-02-19 11:14:36 +0100

    pyroname fini

---
+++ backend/app/Classes/PyromaneFactory.ts
@@ -0,0 +1,17 @@
+import Pyromane from "./Fire";
+
+class PyromaneFactory {
+  static create(position: { x: number; y: number }) {
+    const pyromane = new Pyromane();
+    pyromane.positionsList = [position];
+   
+    return pyromane;
+  }
+
+  static update(pyromane: Pyromane, newPosition: { x: number; y: number }) {
+    const { positionsList } = pyromane;
+    positionsList.push(newPosition);
+  }
+}
+
+export default PyromaneFactory;
