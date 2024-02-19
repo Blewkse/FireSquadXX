e698ddb97060ea7726cf67c8769cdbf7ee03abcb
Renaud-HUSSON <renaudhusson09@gmail.com>
2024-02-19 11:04:54 +0100

    fix: eslint errors

--- backend/app/Classes/Robots/Extincteur.ts
+++ backend/app/Classes/Robots/Extincteur.ts
@@ -1,14 +1,14 @@
 import Robot from './Robot.js'
-import { robotState, robotType } from './enumRobot.js'
+import { RobotType } from './enumRobot.js'
 
 class Extincteur extends Robot {
-  constructor(id: number) {
+  constructor(id: string) {
     super(id)
     this.speed = 50
     this.canPutOut = true
     this.capacity = 250
     this.waterLvL = 250
-    this.type = robotType.extincteur
+    this.type = RobotType.extincteur
   }
 }
 
