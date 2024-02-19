2737506d08a788514ea61f76ea7d586ee59e134d
Blewkse <julesbloux@gmail.com>
2024-02-19 14:47:56 +0100

    rebase

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,6 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
-import { MapComponent } from "./components/map/mapw.component";
-import { HomeComponent } from "./components/home/home/home.component";
-=======
 import { MapComponent } from './components/map/map.component';
 import { HomeComponent } from './components/home/home/home.component';
->>>>>>> 21ca3157f10923a544d661acbd3f5362d7c59fac
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
