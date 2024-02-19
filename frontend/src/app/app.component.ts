5fdb3488029ea5d82402f31e7f229209a1852cac
brice.matias.e <brice.matias.e@thalesdigital.io>
2024-02-19 09:00:15 +0100

    update project

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MapComponent } from "./components/map/map.component";
+import { NavbarComponent } from "./components/navbar/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MapComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MapComponent,
+    NavbarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- frontend/src/app/components/map/map.component.ts
+++ frontend/src/app/components/map/map.component.ts
@@ -1,25 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { interval, Observable } from "rxjs";
+import { NavbarComponent } from "../navbar/navbar/navbar.component";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  standalone: true,
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
+  imports: [
+    NavbarComponent,
+    RouterOutlet
+  ],
+  standalone: true
 })
 export class MapComponent implements OnInit {
-
-  private x: number = 250;
-  private y: number = 250;
-  private dx: number = 2;
-  private dy: number = 1;
+  private points: { x: number; y: number; radius: number; targetRadius: number; growthSpeed: number }[] = [];
 
   ngOnInit(): void {
-    this.drawBackground();
-    this.movePointEvery10Seconds();
+    this.initializeMap();
+    this.addClickEventListener();
+    this.startPointGrowth();
   }
 
-  drawBackground() {
+  initializeMap() {
     const canvas = document.getElementById('mapCanvas') as HTMLCanvasElement;
     const context = canvas?.getContext('2d');
 
@@ -29,48 +31,63 @@ export class MapComponent implements OnInit {
 
       img.onload = () => {
         context.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-        const x = 250;
-        const y = 250;
-
-        context.beginPath();
-        context.arc(x, y, 5, 0, 2 * Math.PI);
-        context.fillStyle = 'red';
-        context.fill();
-        context.closePath();
+        this.drawPoints();
       };
 
       img.addEventListener('error', (event) => {
-        console.error("Error loading image:", event);
+        console.error('Error loading image:', event);
       });
     } else {
-      console.error("Canvas element or context not found.");
+      console.error('Canvas element or context not found.');
     }
   }
 
-
-  movePoint() {
+  drawPoint(x: number, y: number, radius: number) {
     const canvas = document.getElementById('mapCanvas') as HTMLCanvasElement;
     const context = canvas?.getContext('2d');
 
     if (canvas && context) {
-      this.x += this.dx;
-      this.y += this.dy;
-
       context.beginPath();
-      context.arc(this.x, this.y, 5, 0, 2 * Math.PI);
+      context.arc(x, y, radius, 0, 2 * Math.PI);
       context.fillStyle = 'red';
       context.fill();
       context.closePath();
     }
   }
 
-  movePointEvery10Seconds() {
-    const moveInterval: Observable<number> = interval(100);
-    moveInterval.subscribe(() => {
-      this.movePoint();
-    });
+  drawPoints() {
+    for (const point of this.points) {
+      this.drawPoint(point.x, point.y, point.radius);
+    }
   }
 
+  addClickEventListener() {
+    const canvas = document.getElementById('mapCanvas') as HTMLCanvasElement;
 
+    if (canvas) {
+      canvas.addEventListener('click', (event) => {
+        const x = event.clientX - canvas.getBoundingClientRect().left;
+        const y = event.clientY - canvas.getBoundingClientRect().top;
+        const minRadius = 5; // Taille minimale du point
+        const maxRadius = 80; // Taille maximale du point
+        const radius = Math.random() * (minRadius - 1) + 1; // Taille aléatoire entre 1 et minRadius
+        const targetRadius = Math.random() * (maxRadius - minRadius) + minRadius; // Taille aléatoire entre minRadius et maxRadius
+        const growthSpeed = 1; // Vitesse de croissance du point
+
+        this.points.push({ x, y, radius, targetRadius, growthSpeed });
+        this.drawPoints(); // Ajout pour redessiner les points immédiatement après un clic
+      });
+    }
+  }
+
+  startPointGrowth() {
+    setInterval(() => {
+      for (const point of this.points) {
+        if (point.radius < point.targetRadius) {
+          point.radius += point.growthSpeed;
+        }
+      }
+      this.drawPoints();
+    }, 200);
+  }
 }
