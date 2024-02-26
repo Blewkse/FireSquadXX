e25758b173238c4465d3b2097393ede166d09fac
El Luco <saintsuperylucas@gmail.com>
2024-02-26 17:00:52 +0100

    2 canva

--- frontend/src/app/Components/map/map.component.ts
+++ frontend/src/app/Components/map/map.component.ts
@@ -7,6 +7,7 @@ import {
 } from '@angular/core';
 import { MapService } from '../../services/map-service.service';
 import { NavbarComponent } from '../navbar/navbar/navbar.component';
+import { Subscription, interval } from 'rxjs';
 
 type Tile = {
   color: string;
@@ -23,8 +24,12 @@ type Tile = {
 })
 export class MapComponent implements AfterViewInit {
   mapTiles: Tile[][] = [];
-  @ViewChild('mapCanvas') mapCanvas!: ElementRef<HTMLCanvasElement>;
+  transMapTiles: Tile[][] = [];
 
+  @ViewChild('mapCanvas') mapCanvas!: ElementRef<HTMLCanvasElement>;
+  @ViewChild('mapCanvasTransparent') mapCanvasTransparent!: ElementRef<HTMLCanvasElement>;
+
+  canvasUpdateSubscription: Subscription = new Subscription();
   constructor(private mapService: MapService) {}
   ngAfterViewInit(): void {
     this.mapService.getMap().subscribe((data) => {
@@ -33,11 +38,51 @@ export class MapComponent implements AfterViewInit {
     });
   }
 
+  onSecondCanvasClick(): void {
+    const canvas: HTMLCanvasElement = this.mapCanvasTransparent.nativeElement;
+    const context = canvas.getContext('2d');
+    canvas.width = canvas.height = 750;
+    // Dessinez quelque chose sur le deuxième canvas (pour illustration)
+    if (context) {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.fillStyle ='black'; // Provide a default value for the fillStyle property
+      context.fillRect(1, 1, 10, 10);
+    }
+  }
+
+  getPixelColorFromFirstCanvas(x: number, y: number): string | undefined {
+    const canvas: HTMLCanvasElement = this.mapCanvas.nativeElement;
+    const context = canvas.getContext('2d');
+    if (context) {
+      // Récupérer les données du pixel sur le premier canvas
+      const pixelData = context.getImageData(x, y, 1, 1).data;
+
+      // Convertir les données RGBA en une chaîne de couleur CSS
+      const color = `rgb(${pixelData[0]}, ${pixelData[1]}, ${pixelData[2]})`;
+      return color;
+    }
+    return undefined;
+  }
+
+
+drawMapTrans(array:any[], x:number,y:number): void {
+  const canvas = this.mapCanvasTransparent.nativeElement;
+  const ctx = canvas.getContext('2d');
+  if (!ctx) return;
+
+  array.forEach((row, i) => {
+    ctx.fillStyle = row.color;
+    ctx.fillRect(row.x, row.y, 4,4);
+
+
+  });
+ 
+}
+
   drawMap(): void {
     const canvas = this.mapCanvas.nativeElement;
     const ctx = canvas.getContext('2d');
     canvas.width = canvas.height = 750;
-
     if (!ctx) return;
 
     const tileSize = 4;
@@ -50,5 +95,6 @@ export class MapComponent implements AfterViewInit {
         ctx.fillRect(j * tileSize, i * tileSize, tileSize, tileSize);
       }
     }
+   
   }
 }
